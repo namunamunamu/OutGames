2665bc3582c8695e79240f0068854839ae09a8a0
kencoor9 <kencoor9@gmail.com>
2025-06-10 16:52:31 +0900

    유니티 프로젝트 커밋

---
+++ Assets/EnemySpanwer.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Redcode.Pools;
+using Unity.FPS.AI;
+using UnityEngine;
+
+public class EnemySpanwer : MonoBehaviour
+{
+    public List<Transform> SpawnPoints;
+    public GameObject EnemyPrefab;
+
+    private float _currentTime;
+    private const float REWPAWN_TIME = 3f;
+    private const int MAX_COUNT = 10;
+
+
+    private void Update()
+    {
+        _currentTime += Time.deltaTime;
+
+        if (_currentTime >= REWPAWN_TIME)
+        {
+            _currentTime = 0f;
+
+            int enemyCount = FindObjectsByType<EnemyController>(FindObjectsSortMode.None).Length;
+            if (enemyCount >= MAX_COUNT)
+            {
+                return;
+            }
+
+            var randomIndex = Random.Range(0, SpawnPoints.Count);
+            Instantiate(EnemyPrefab, SpawnPoints[randomIndex].position, Quaternion.identity);
+            // EnemyController enemy = PoolManager.Instance.enemyMobilePool.Get();
+            // enemy.transform.position = SpawnPoints[randomIndex].position;
+        }
+    }
+
+}
